09e30bfe288cee937b0abf37bfab8648d081d54e
Anil Choudhary <anilkhokhar1151@gmail.com>
2023-07-14 12:00:47 +0530

    modify middle part of website

--- src/components/Services.js
+++ src/components/Services.js
@@ -12,7 +12,7 @@ function Services() {
     <div className='services'>
 
         <div className='Box_1'>
-            <h1>What<span>we do</span> for our customers</h1>
+             What<span> we do</span> for our customers
         </div>
 
         <div className='Box_2'>
@@ -22,8 +22,8 @@ function Services() {
             <div className='Box_2_2'>
                 <h1>We are a bunch of tech geeks who are trying to change the way how technology is implemented with the use of web3, cloud, automation, & blockchain, We constantly try to outdo ourselves & deliver the best outcomes for you.</h1>
             </div>
-            <div className='Box_2_3'>
-                <h1>”</h1>
+            <div className='Box_2_1'>
+                <h1>“</h1>
             </div>
         </div>
 
@@ -35,32 +35,32 @@ function Services() {
                 <div className='text_1'><h2>Web Development</h2></div>
             </div>
 
-            <div className='Box_3_2'>
+            <div className='Box_3_1'>
                 <h1><img src={Ellipse14} alt='img'/></h1>
                 <div className='text_1'><h2>App Development</h2></div>
             </div>
             
-            <div className='Box_3_3'>
+            <div className='Box_3_1'>
                 <h1><img src={Ellipse15} alt='img'/></h1>
                 <div className='text_1'><h2>Devops Development</h2></div>
             </div>
             
-            <div className='Box_3_4'>
+            <div className='Box_3_1'>
                 <h1><img src={Ellipse16} alt='img'/></h1>
                 <div className='text_1'><h2>E-Commerce Solutions</h2></div>
             </div>
             
-            <div className='Box_3_5'>
+            <div className='Box_3_1'>
                 <h1><img src={Ellipse17} alt='img'/></h1>
                 <div className='text_1'><h2>Digital Transformation</h2></div>
             </div>
             
-            <div className='Box_3_6'>
+            <div className='Box_3_1'>
                 <h1><img src={Ellipse18} alt='img'/></h1>
                 <div className='text_1'><h2>Cloud Computing</h2></div>
             </div>
             
-            <div className='Box_3_7'>
+            <div className='Box_3_1'>
                 <h1><img src={Ellipse19} alt='img'/></h1>
                 <div className='text_1'><h2>Database Management</h2></div>
             </div>
