36f9b9046565036479c166ea8c048f2ca707c70d
Akbar Khan <akbar29052@gmail.com>
2023-07-15 12:45:52 +0530

    top edit

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -37,9 +37,6 @@ const Banner = () => {
             alt="Second slide"
           />
           <div style={{ position: 'absolute', bottom: 0, color: 'white', height: 400, marginLeft: 150 }}>
-            <h1>Games</h1>
-            <p>Play your favorite esports titles and crush your opponents in daily tournaments,
-              exciting leagues and thrilling challenges.</p>
             <a href="/home" className='hero-button' target="_blank">
               <span> </span>
               <span> </span>
@@ -58,15 +55,6 @@ const Banner = () => {
             alt="Third slide"
           />
           <div style={{ position: 'absolute', bottom: 0, color: 'white', height: 400, marginLeft: 150 }}>
-            <h1>Treasure</h1>
-            <p>Compete for prize money and other digital prizes - Team or Solo.</p>
-            <a href="/home" className='hero-button' target="_blank">
-              <span> </span>
-              <span> </span>
-              <span> </span>
-              <span> </span>
-              JOIN AKM
-              </a>
           </div>
         </Carousel.Item>
       </Carousel>
