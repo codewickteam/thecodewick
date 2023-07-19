661eb007c91beed5d176285d885ed20c042a956b
Kushal Kumawat <Kushalkumawat102@gmail.com>
2023-07-19 12:50:53 +0530

    middle about page created

--- src/components/AboutUs/MiddleAbout.js
+++ src/components/AboutUs/MiddleAbout.js
@@ -1,9 +1,18 @@
 import React from 'react'
+import './MiddleAbout.css'
 
 const MiddleAbout = () => {
   return (
-    <div>
-      middle
+    <div className='middle'>
+      
+      <div className='box_1'>
+      <h1>Why Choose Us</h1>
+      </div>
+
+      <div className='box_2'>
+        <h2>Thank you </h2>      
+      </div>      
+    
     </div>
   )
 }
