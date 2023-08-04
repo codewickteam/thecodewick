f05f958c2b6a974a9d7fd4d78e71b3f7a7a3758c
Akbar Khan <akbar29052@gmail.com>
2023-08-04 10:58:53 +0530

    mobile nav

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,12 +1,43 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { styled } from 'styled-components'
 import wicklogo from '../assets/codewick-logo.png'
 import { Link } from 'react-router-dom'
 
 export default function Navbar() {
+  const[show, setShow] = useState(false)
+
   return (
     <Container>
       <div className="container flex a-center j-between">
+      <div onClick={()=> setShow(!show)} className="m-logo">
+            { show ? 
+                (
+                <Link className="mlogo-icon2">
+                    <div className='mlogo-top2'></div>
+                    <div className='mlogo-bottom2'></div>
+                </Link>
+                ) : (
+            <Link className="mlogo-icon1">
+                <div className='mlogo-top1'></div>
+                <div className='mlogo-bottom1'></div>
+            </Link>
+                )
+            }
+
+            { show &&
+                <div className="mlogo-menu flex f-column a-center j-center">
+                <Link to='/'>HOME</Link>
+                <di className="rule"/>
+                <Link to='/'>ABOUT US</Link>
+                <div className="rule"/>
+                <Link to='/'>BLOG</Link>
+            </div>
+            }
+        </div>
+
+
+
+
         <div className="logo box">
         <Link to='/' className='logo'><img src={wicklogo} alt="" className="logo" /></Link>
         </div>
@@ -36,9 +67,10 @@ const Container = styled.div`
   padding-top: 59px;
   display: flex;
 
-.container{
-  max-width: 1440px;
-}
+  .container{
+    /* max-width: 1440px; */
+    display: none;
+  }
   .flex{
     display: flex;
   }
@@ -47,6 +79,9 @@ const Container = styled.div`
   }
   .j-center{
     justify-content: center;
+  }
+  .f-column{
+    flex-direction: column;
   }
   .j-evenly{
     justify-content: space-evenly;
@@ -63,6 +98,7 @@ const Container = styled.div`
   .logo{
     width: 220px;
     height: 42px;
+    /* display: none; */
     /* margin-right: 149px; */
   }
   .link{
@@ -98,6 +134,103 @@ const Container = styled.div`
     font-style: normal;
     font-weight: 700;
     padding: 8px 38px;
+  }
+
+
+//mobile navbar
+
+.mlogo-icon1{
+    position: absolute;
+    z-index: 999;
+    top: 2.25rem;
+    left: 0.94rem;
+  }
+  .mlogo-icon2{
+    position: absolute;
+    z-index: 999;
+    top: 2.19rem;
+    left: 0.94rem;
+  }
+  .mlogo-top1{
+    width: 2.5rem;
+    height: 1rem;
+    flex-shrink: 0;
+    border-radius: 1.25rem;
+    background: #000;
+    margin-bottom: 0.25rem;
+  }
+  .mlogo-bottom1{
+    width: 1.5625rem;
+    height: 1rem;
+    flex-shrink: 0;
+    border-radius: 1.25rem;
+    background: #000;
+  }
+  .mlogo-top2{
+    width: 1.25rem;
+    height: 0.5rem;
+    flex-shrink: 0;
+    border-radius: 1.25rem;
+    background: white;
+    margin-bottom: 0.25rem;
+  }
+  .mlogo-bottom2{
+    width: 0.75rem;
+    height: 0.5rem;
+    flex-shrink: 0;
+    border-radius: 1.25rem;
+    background: white;
+  }
+
+  .mlogo-menu{
+    width: 13.75rem;
+    height: 11.375rem;
+    top: 0;
+    left: 0;
+    border-radius: 0 0 15px 0;
+    position: absolute;
+    background-color: #000;
+  }
+
+  .rule{
+    width: 6.875rem;
+    height: 0.0625rem;
+    background: #FFF;
+    margin: 0.6rem 0;
+  }
+
+  a{
+    color: #FFF;
+  }
+
+  @media (min-width: 1080px){
+    .m-logo{
+      display: none;
+    }
+  }
+  @media (max-width: 1080px){
+    .logo, .links, .btn{
+      display: none;
+    }
+  }
+  @media (max-width: 1025px){
+    /* .logo, .links, .btn{
+      display: none;
+    } */
+    /* padding-top: 54px; 
+    height: 365px;  */
+  }
+  @media (max-width: 770px){
+    /* padding-top: 54px; 
+    height: 290px;  */
+  }
+  @media (max-width: 430px){
+    /* padding-top: 24px; 
+    height: 200px;  */
+  }
+  @media (max-width: 390px){
+    /* padding-top: 10px; 
+    height: 139px;  */
   }
 
 
