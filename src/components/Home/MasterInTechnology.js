cad296eaeaacdc73dcc5cb3b04b1725ee067e597
Anil Choudhary <134296442+AnilChoudhary1151@users.noreply.github.com>
2023-08-05 15:07:18 +0530

    update trending technology homepage

--- src/components/Home/MasterInTechnology.js
+++ src/components/Home/MasterInTechnology.js
@@ -1,48 +1,90 @@
 import React from 'react';
 import styled  from 'styled-components';
-import image4 from '../../assets/NodeJS logo.png';
-import image5 from '../../assets/image5.png';
-import image6 from '../../assets/React_JS Developer - Web_Mobile UI_UX Design & Development.png';
-import image7 from '../../assets/image7.png';
-import image8 from '../../assets/image8.png';
-import image9 from '../../assets/image9.png';
-import image10 from '../../assets/image10.png';
-import image11 from '../../assets/image11.png';
+import image1 from '../../assets/NodeJS logo.png';
+import image2 from '../../assets/saas.png';
+import image3 from '../../assets/Java.png' ;
+import image4 from '../../assets/Python.png' ;
+import image5 from '../../assets/React_JS Developer - Web_Mobile UI_UX Design & Development.png' ;
+import image6 from '../../assets/octagen.png' ;
+import image7 from '../../assets/Salesforce.png' ;
+import image8 from '../../assets/tensorflow.png' ;
+import image9 from '../../assets/jackines.png' ;
+import image10 from '../../assets/studio.png' ;
+import image11 from '../../assets/docker.jpg' ;
+import image12 from '../../assets/mdb.png' ;
+import image13 from '../../assets/aws.png' ;
+import image14 from '../../assets/image6.png' ;
+import image15 from '../../assets/image7.png' ;
+import image16 from '../../assets/image11.png' ;
+
+
 
 
 const data=[
     {
         id:"p1",
-        imageUrl:image4,
+        imageUrl:image1,
     },
     {
         id:"p2",
-        imageUrl:image5,
+        imageUrl:image2,
     },
     {
         id:"p3",
-        imageUrl:image6,
+        imageUrl:image3,
     },
     {
         id:"p4",
-        imageUrl:image7,
+        imageUrl:image4,
     },
     {
         id:"p5",
-        imageUrl:image11,
+        imageUrl:image5,
     },
     {
         id:"p6",
-        imageUrl:image10,
+        imageUrl:image6,
     },
     {
         id:"p7",
-        imageUrl:image9,
+        imageUrl:image7,
     },
     {
         id:"p8",
         imageUrl:image8,
     },
+    {
+      id:"p9",
+      imageUrl:image9,
+  },
+  {
+      id:"p10",
+      imageUrl:image10,
+  },
+  {
+      id:"p11",
+      imageUrl:image11,
+  },
+  {
+      id:"p12",
+      imageUrl:image12,
+  },
+  {
+      id:"p13",
+      imageUrl:image13,
+  },
+  {
+      id:"p14",
+      imageUrl:image14,
+  },
+  {
+      id:"p15",
+      imageUrl:image15,
+  },
+  {
+      id:"p16",
+      imageUrl:image16,
+  },
 ];
 
 
@@ -74,7 +116,7 @@ const Container = styled.div`
   display: flex;
   flex-direction: column;
   margin-top: 100px;
-  @media (max-width: 1025px){
+  /* @media (max-width: 1025px){
     max-height: 800px;
   }
   @media (max-width: 770px){
@@ -85,7 +127,7 @@ const Container = styled.div`
   }
   @media (max-width: 390px){
     max-height: 400px;
-  }
+  } */
 `;
 
 const Header = styled.div`
@@ -186,15 +228,18 @@ const Header = styled.div`
 `;
 
 const CardsContainer = styled.div`
-  display: grid;
-  grid-template-columns: repeat(4, 1fr);
-  justify-items: center;
-  gap: 100px;
+  display: flex;
+  justify-content: space-evenly;
+  flex-wrap: wrap;
+  gap:2vw;
   margin-bottom: 50px;
   margin-top: 100px;
   img {
-  width: 10vw;
-  height: 10vw;
+  width: 9vw;
+  height: 9vw;
+  padding:4px;
+  border: 2px solid rgb(168, 166, 166);
+  border-radius: 0.6rem;
   cursor: pointer;
   transition: transform 0.3s ease; 
 }
